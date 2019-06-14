7f2a14a791e0c5dc82aae60289a577dae3e76046
Jun Nagai <junna9a1@gmail.com>
2019-06-14 10:32:26 +0800

    [[git-now]] Fri Jun 14 10:32:26 CST 2019
     mix.exs | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Struct.MixProject do
 
   def project do
     [
-      app: :struct,
+      app: :cs_struct,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
